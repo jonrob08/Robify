0e8acf41f4136c99e11e8d156cdad7fceaddfc46
Jon R <jonrob08@gmail.com>
2022-05-26 11:23:50 -0400

    Sidebar elements
    Adding in some dummy left sidebar elements.

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,20 +11,16 @@ import {
 function Sidebar() {
     return (
         <div className="text-gray-600 p-5 text-sm border-r border-gray-900">
-            
-            <div>
+            <div className="space-y-4">
                 <button className="flex items-center space-x-2 hover:text-white">
                     <HomeIcon className="h-5 w-5"/>
                     <p>Home</p>
                 </button>
-            </div>
-            <div>
                 <button className="flex items-center space-x-2 hover:text-white">
                     <SearchIcon className="h-5 w-5"/>
                     <p>Search</p>
                 </button>
-            </div>
-            <div>
+
                 <button className="flex items-center space-x-2 hover:text-white">
                     <LibraryIcon className="h-5 w-5"/>
                     <p>Your Library</p>
@@ -35,19 +31,39 @@ function Sidebar() {
                     <PlusCircleIcon className="h-5 w-5"/>
                     <p>Create Playlist</p>
                 </button>
-            </div>
-            <div>
+ 
                 <button className="flex items-center space-x-2 hover:text-white">
                     <HeartIcon className="h-5 w-5"/>
                     <p>Liked Songs</p>
                 </button>
-            </div>
-            <div>
+
                 <button className="flex items-center space-x-2 hover:text-white">
                     <RssIcon className="h-5 w-5"/>
                     <p>Your Episodes</p>
                 </button>
                 <hr className="border-t-[0.1px] border-gray-900"></hr>
+
+                {/* Playlists... */}
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+                <p className="cursor-pointer hover:text-white">Playlist Name...</p>
             </div>
         </div>
         
